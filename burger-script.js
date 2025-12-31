21d069bff6f51eb1c12725be3ef44ac167261777
Alex <bahler831@gmail.com>
2025-12-30 20:36:37 -0800

    Fixed policy page email link

--- burger-script.js
+++ burger-script.js
@@ -12,3 +12,11 @@ document.querySelectorAll(".nav-link").forEach(link =>
     navMenu.classList.remove("active");
   })
 );
+
+window.addEventListener("load", (event) => {
+    const emailLinks = document.querySelectorAll('a[href^="mailto:"]');
+    emailLinks.forEach(link => {
+        console.log(link.innerText);
+        link.href = link.href + link.innerText;
+    });
+});
